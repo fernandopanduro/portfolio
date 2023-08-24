cb52d79d44968cc0ff714b078082261fb8733ac1
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-08-24 08:23:00 -0700

    ✨ feat: Create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Achievements } from "./pages/Achievements";
+import { Contact } from "./pages/Contact";
+import { Experience } from "./pages/Experience";
+import { Home } from "./pages/Home";
+import { Portfolio } from "./pages/Portfolio";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>fer p.</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/achievements" element={<Achievements />} />
+
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </>
---
+++ src/pages/Achievements.tsx
@@ -0,0 +1,3 @@
+export const Achievements = () => {
+  return <div>Achievements</div>;
+};
