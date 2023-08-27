2ad92760dbedcb71c789d1898eee3dc59c4ba815
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-08-27 15:19:24 -0700

    ✨ feat: Add section hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import { Dm } from "../components/Dm";
+import { ArrowDown } from "../components/Icons";
 import styles from "../css/Home.module.css";
 
 export const Home = () => {
   return (
     <div>
+      <section className={`container ${styles.hero}`}>
+        <h1>Fernando Panduro</h1>
+        <h2 className={`${styles.heroTitle}`}>
+          I’m a Mexican Software Engineer based ↔ Sinaloa, MX passionate about
+          shaping how humans interact with technology.
+        </h2>
+        <div>
+          <a className={`${styles.arrowDown}`} href="#portfolio">
+            <ArrowDown />
+          </a>
+        </div>
+      </section>
       <section className="container">
         <h3 className={styles.experienceTitle}>Past & Present</h3>
         <article className={`${styles.experience}`}>
