408dd77c9c1df10fccac086522c1632c97f7b833
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-08-27 18:49:03 -0700

    ✨ feat: Add section portfolio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,142 @@ export const Home = () => {
           </a>
         </div>
       </section>
-      <section className="container">
+      <section className={`container ${styles.portfolio}`}>
+        <h2 className={styles.experienceTitle}>Portfolio</h2>
+        <article className={`${styles.portfolioSection}`}>
+          <aside className={`${styles.portfolioAside}`}>
+            <h3 className={`${styles.portfolioTitle}`}>MG Motor</h3>
+            <p className={`${styles.portfolioDescription}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              quibusdam. Corporis illo nisi, quidem sit quaerat quasi alias quod
+              eos adipisci ipsam ad magnam commodi enim perspiciatis
+              voluptatibus. Aperiam, natus?
+            </p>
+            <div className={`${styles.portfolioButtons}`}>
+              <a
+                href="$2"
+                title="View more"
+                aria-label="View more"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                View more
+              </a>
+              <a
+                href="https://www.mgmotor.com.mx/"
+                title="MG Motor"
+                aria-label="MG Motor"
+                target="_blank"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                Website
+              </a>
+            </div>
+          </aside>
+          <picture className={`${styles.portfolioImage}`}>
+            <img
+              src="src/assets/images/mockups/mg-mockup-labtop.png"
+              alt="Mockup Labtop MG"
+              title="Mockup Labtop MG"
+              aria-label="Mockup Labtop MG"
+              className="$3"
+              draggable="false"
+              loading="lazy"
+              decoding="async"
+              width="450"
+              height="300"
+            />
+          </picture>
+        </article>
+        <article className={`${styles.portfolioSection}`}>
+          <aside className={`${styles.portfolioAside}`}>
+            <h3 className={`${styles.portfolioTitle}`}>Kia México</h3>
+            <p className={`${styles.portfolioDescription}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              quibusdam. Corporis illo nisi, quidem sit quaerat quasi alias quod
+              eos adipisci ipsam ad magnam commodi enim perspiciatis
+              voluptatibus. Aperiam, natus?
+            </p>
+            <div className={`${styles.portfolioButtons}`}>
+              <a
+                href="$2"
+                title="View more"
+                aria-label="View more"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                View more
+              </a>
+              <a
+                href="https://www.kia.com/mx/main.html/"
+                title="Kia Mexico"
+                aria-label="Kia Mexico"
+                target="_blank"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                Website
+              </a>
+            </div>
+          </aside>
+          <picture className={`${styles.portfolioImage}`}>
+            <img
+              src="src/assets/images/mockups/kia-mockup-phone.png"
+              alt="Mockup Phone Kia"
+              title="Mockup Phone Kia"
+              aria-label="Mockup Phone Kia"
+              className="$3"
+              draggable="false"
+              loading="lazy"
+              decoding="async"
+              width="450"
+              height="300"
+            />
+          </picture>
+        </article>
+        <article className={`${styles.portfolioSection}`}>
+          <aside className={`${styles.portfolioAside}`}>
+            <h3 className={`${styles.portfolioTitle}`}>Chirey México</h3>
+            <p className={`${styles.portfolioDescription}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              quibusdam. Corporis illo nisi, quidem sit quaerat quasi alias quod
+              eos adipisci ipsam ad magnam commodi enim perspiciatis
+              voluptatibus. Aperiam, natus?
+            </p>
+            <div className={`${styles.portfolioButtons}`}>
+              <a
+                href="$2"
+                title="View more"
+                aria-label="View more"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                View more
+              </a>
+              <a
+                href="https://www.chirey.mx/"
+                title="Chirey"
+                aria-label="Chirey"
+                target="_blank"
+                rel="noreferrer noopener"
+                className={`${styles.portfolioButton}`}>
+                Website
+              </a>
+            </div>
+          </aside>
+          <picture className={`${styles.portfolioImage}`}>
+            <img
+              src="src/assets/images/mockups/chirey-mockup-tablet.png"
+              alt="Mockup Tablet Chirey"
+              title="Mockup Tablet Chirey"
+              aria-label="Mockup Tablet Chirey"
+              className="$3"
+              draggable="false"
+              loading="lazy"
+              decoding="async"
+              width="450"
+              height="300"
+            />
+          </picture>
+        </article>
+      </section>
+      <section id="portfolio" className="container">
         <h3 className={styles.experienceTitle}>Past & Present</h3>
         <article className={`${styles.experience}`}>
           <div className={`${styles.experienceDescription}`}>
